77449a7008915b0af421a7c655998a55522345da
Alex-V-Kovalev <a.kovalev@mail.ru>
2021-07-10 14:00:52 +0300

    удалил все лишнее

--- src/main/java/web/spring_boot_security_bootstrap/controller/UserController.java
+++ src/main/java/web/spring_boot_security_bootstrap/controller/UserController.java
@@ -13,36 +13,24 @@ import web.spring_boot_security_bootstrap.service.UserService;
 @Controller
 @RequestMapping("/")
 public class UserController {
-	private UserService userService;
+    private UserService userService;
 
-	@Autowired
-	public void setUserService(UserService userService) {
-		this.userService = userService;
-	}
+    @Autowired
+    public void setUserService(UserService userService) {
+        this.userService = userService;
+    }
 
-	@GetMapping(value = "user")
-	public String getUserPage(ModelMap model, @AuthenticationPrincipal User authUser) {
-		model.addAttribute("user", userService.getUserByEmail(SecurityContextHolder.getContext().getAuthentication().getName()));
-		model.addAttribute("authUser", authUser);
-		return "main-view";
-	}
+    @GetMapping("")
+    public String start() {
+        return "main-view";
+    }
 
-//	@GetMapping(value = "user/{id}")
-//	public String getUserPageById(ModelMap model, @PathVariable("id") Long id) {
-//		model.addAttribute("user", userService.getUserById(id));
-////		System.out.println(userService.getUserById(2L));
-//		return "user-view";
-//	}
-//
-//	@GetMapping(value = "hello")
-//	public String printWelcome(ModelMap model) {
-//		List<String> messages = new ArrayList<>();
-//		messages.add("Hello!");
-//		messages.add("I'm Spring MVC-SECURITY application");
-//		messages.add("5.2.0 version by sep'19 ");
-//		model.addAttribute("messages", messages);
-//		return "hello";
-//	}
+    @GetMapping(value = "user")
+    public String getUserPage(ModelMap model, @AuthenticationPrincipal User authUser) {
+        model.addAttribute("user", userService.getUserByEmail(SecurityContextHolder.getContext().getAuthentication().getName()));
+        model.addAttribute("authUser", authUser);
+        return "main-view";
+    }
 
     @GetMapping(value = "login")
     public String loginPage() {
