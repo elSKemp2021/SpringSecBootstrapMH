9c7465e41a19be96c3f1f670404c9dcb5f5c37b4
Alex-V-Kovalev <a.kovalev@mail.ru>
2021-07-10 14:00:16 +0300

    application.properties - добавлена поддержка hidden method AdminController - переписал методы и маппинги

--- src/main/java/web/spring_boot_security_bootstrap/controller/AdminController.java
+++ src/main/java/web/spring_boot_security_bootstrap/controller/AdminController.java
@@ -3,9 +3,8 @@ package web.spring_boot_security_bootstrap.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-
 import web.spring_boot_security_bootstrap.entity.Role;
 import web.spring_boot_security_bootstrap.entity.User;
 import web.spring_boot_security_bootstrap.service.RoleService;
@@ -16,62 +15,72 @@ import java.util.List;
 @Controller
 @RequestMapping("/admin")
 public class AdminController {
-	private UserService userService;
-	private RoleService roleService;
+    private UserService userService;
+    private RoleService roleService;
 
-	@Autowired
-	public void setUserService(UserService userService) {
-		this.userService = userService;
-	}
+    @Autowired
+    public void setUserService(UserService userService) {
+        this.userService = userService;
+    }
 
-	@Autowired
-	public void setRoleService(RoleService roleService) {
-		this.roleService = roleService;
-	}
+    @Autowired
+    public void setRoleService(RoleService roleService) {
+        this.roleService = roleService;
+    }
 
 
-    @GetMapping(value = "")
-	public String getUsersPage(ModelMap model, @AuthenticationPrincipal User authUser) {
-		List<User> users = userService.getAllUsers();
-		List<Role> roles = roleService.getAllRoles();
-		model.addAttribute("usersList", users);
-		model.addAttribute("rolesList", roles);
-		model.addAttribute("authUser", authUser);
-		return "main-view";
-	}
+    @GetMapping("")
+    public String getUsersPage(Model model, @AuthenticationPrincipal User authUser) {
+        List<User> users = userService.getAllUsers();
+        List<Role> roles = roleService.getAllRoles();
+        model.addAttribute("usersList", users);
+        model.addAttribute("rolesList", roles);
+        model.addAttribute("newUser", new User());
+        model.addAttribute("authUser", authUser);
+        return "main-view";
+    }
 
-	@GetMapping(value = "/add")
-	public String addUserPage(ModelMap model) {
-		List<Role> rolesList = roleService.getAllRoles();
-		model.addAttribute("rolesList", rolesList);
-		model.addAttribute("user", new User());
-		return "main-view#new-user";
-	}
+    @PostMapping("/add")
+    public String addUserPage(@ModelAttribute User user) {
+        userService.create(user);
+        return "redirect:/admin";
+    }
 
-//	@PostMapping(value = "/add")
-//	public String addUser(@ModelAttribute("user") User user) {
-//		userService.create(user);
-//		return "redirect:/admin";
-//	}
-//
-//	@GetMapping(value = "/edit/{id}")
-//	public String editUserPage(ModelMap model, @PathVariable("id") long id) {
-//		List<Role> rolesList = roleService.getAllRoles();
-//		model.addAttribute("rolesList", rolesList);
-//		model.addAttribute("user", userService.getUserById(id));
-//		return "useredit-view";
-//	}
-//
-//	@PostMapping(value = "/edit")
-//	public String updateUser(@ModelAttribute("user") User user) {
-//		userService.update(user);
-//		return "redirect:/admin";
-//	}
-//
-//	@GetMapping(value = "/delete/{id}")
-//	public String deleteUser(@PathVariable("id") long id) {
-//		userService.delete(userService.getUserById(id));
-//		return "redirect:/admin";
-//	}
+    @GetMapping("/edit/{id}")
+    public String editUserView(Model model, @PathVariable long id) {
+        List<Role> rolesList = roleService.getAllRoles();
+        model.addAttribute("rolesList", rolesList);
+        model.addAttribute("user", userService.getUserById(id));
+        return "useredit-modal";
+    }
+
+    @PatchMapping("/edit")
+    public String updateUser(@ModelAttribute User user) {
+        User oldUser = userService.getUserById(user.getId());
+        oldUser.setNameFirst(user.getNameFirst());
+        oldUser.setNameLast(user.getNameLast());
+        oldUser.setEmail(user.getEmail());
+        oldUser.setAge(user.getAge());
+        if ((user.getPassword() != null) || !(user.getPassword().isEmpty())) {
+            oldUser.setPassword(user.getPassword());
+        }
+        oldUser.setRoles(user.getRoles());
+        userService.update(user);
+        return "redirect:/admin";
+    }
+
+    @GetMapping("/delete/{id}")
+    public String deleteUserView(Model model, @PathVariable long id) {
+        List<Role> rolesList = roleService.getAllRoles();
+        model.addAttribute("user", userService.getUserById(id));
+        model.addAttribute("rolesList", rolesList);
+        return "delete-modal";
+    }
+
+    @DeleteMapping("/delete")
+    public String deleteUser(@ModelAttribute User user) {
+        userService.delete(user);
+        return "redirect:/admin";
+    }
 
 }
